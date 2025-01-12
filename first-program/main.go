87ebd7b73cf5dc8a05c0d816c1e358fc7d5607ce
rashedevs <rasheduap2015@gmail.com>
2025-01-13 00:19:24 +0600

    exploring multireturn named and take input

--- first-program/main.go
+++ first-program/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"first-program/practice"
 	"fmt"
 )
 
+// "first-program/practice"
 //variable................................................................
 // func main(){
 // 	// Declare and initialize an int/float32/bool/string variable
@@ -18,34 +18,99 @@ import (
 // 	fmt.Println("hello, world!",a,p)
 // }
 
+
+func add(num1 int, num2 int) int {
+	sum := num1 + num2
+	// fmt.Println("Sum of", num1, "and", num2, "is", sum)
+	return sum
+}
+
+func allOps(num1 int, num2 int) (int, int) {
+	sum := num1 + num2
+	mul := num1 * num2
+	// fmt.Println("Sum of", num1, "and", num2, "is", sum)
+	return sum, mul
+}
+
+func myFunction(x int, y string) (result int, txt1 string) {
+	result = x + x
+	txt1 = y + " World!"
+	return
+  }
+
+
 //if-else or switch................................................................
 func main(){
 
-age :=20
-a :=9
+// age :=20
+// a :=9
 
 //if-else
-if age >18{
-	fmt.Println("You are an adult")
-} else if age < 18 {
-	fmt.Println("You are a minor")
-} else {
-	fmt.Println("You are 18 so wait...")
-}
+// if age >18{
+// 	fmt.Println("You are an adult")
+// } else if age < 18 {
+// 	fmt.Println("You are a minor")
+// } else {
+// 	fmt.Println("You are 18 so wait...")
+// }
 
 //switch
-switch a {
-case 1, 3, 5:
-    fmt.Println("a is 1 or 3 or 5")
-case 2, 4:
-    fmt.Println("a is either 2 or 4")
-case 6, 7:
-    fmt.Println("a is either 6 or 7")
-default:
-    fmt.Println("a is for something else")
-}
-var gunfol  = practice.Gun(30, 30)
-var biyogfol = practice.Biyog(300, 120)
+// switch a {
+// case 1, 3, 5:
+//     fmt.Println("a is 1 or 3 or 5")
+// case 2, 4:
+//     fmt.Println("a is either 2 or 4")
+// case 6, 7:
+//     fmt.Println("a is either 6 or 7")
+// default:
+//     fmt.Println("a is for something else")
+// }
+// var gunfol  = practice.Gun(30, 30)
+// var biyogfol = practice.Biyog(300, 120)
 
-fmt.Print("gun----------", gunfol, "-------biyog--------", biyogfol)
+
+
+// class-10 (functions):
+
+// x :=11
+// y :=33
+
+// result := add(x,y)
+
+// re1, res2 := allOps(x, y)
+
+// d, e := myFunction(10, "Hello")
+// _, z := myFunction(5, "Hi") //ommit y returned value
+
+
+// fmt.Println("result: ", result, re1, res2)
+// fmt.Println(d,e)
+// fmt.Println(z)
+
+
+
+//take input from user
+fmt.Println("Welcome to my application")
+
+
+var userName string
+fmt.Println("Enter your name: ")
+fmt.Scanln(&userName)
+fmt.Println("your name is -----", userName)
+
+var numOne int
+var numTwo int
+fmt.Println("Enter number one: ")
+fmt.Scanln(&numOne)
+
+fmt.Println("Enter number two: ")
+fmt.Scanln(&numTwo)  //by &numTwo we are assigning numTwo to the variable
+
+sum := add(numOne, numTwo)
+fmt.Println("Hi",userName,"Your Sum of two input numbers is: ", sum)
+
+fmt.Println("Thank you for using my application")
+fmt.Println("Good bye")
+
+
 }
