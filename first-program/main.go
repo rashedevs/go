3a409bf755746e255099af8ce1246c8db622afaf
rashedevs <rasheduap2015@gmail.com>
2025-01-19 03:00:13 +0600

    explored scope for var and func

--- first-program/main.go
+++ first-program/main.go
@@ -18,12 +18,11 @@ import (
 // 	fmt.Println("hello, world!",a,p)
 // }
 
-
-func add(num1 int, num2 int) int {
-	sum := num1 + num2
-	// fmt.Println("Sum of", num1, "and", num2, "is", sum)
-	return sum
-}
+// func add(num1 int, num2 int) int {
+// 	sum := num1 + num2
+// 	// fmt.Println("Sum of", num1, "and", num2, "is", sum)
+// 	return sum
+// }
 
 // func allOps(num1 int, num2 int) (int, int) {
 // 	sum := num1 + num2
@@ -37,7 +36,6 @@ func add(num1 int, num2 int) int {
 // 	txt1 = y + " World!"
 // 	return
 //   }
-
 
 //if-else or switch................................................................
 // func main(){
@@ -68,8 +66,6 @@ func add(num1 int, num2 int) int {
 // var gunfol  = practice.Gun(30, 30)
 // var biyogfol = practice.Biyog(300, 120)
 
-
-
 // class-10 (functions):
 
 // x :=11
@@ -82,64 +78,85 @@ func add(num1 int, num2 int) int {
 // d, e := myFunction(10, "Hello")
 // _, z := myFunction(5, "Hi") //ommit y returned value
 
-
 // fmt.Println("result: ", result, re1, res2)
 // fmt.Println(d,e)
 // fmt.Println(z)
 
 //applying S of SOLID................................
 
-func printWelcomeMessage(){
-	fmt.Println("Welcome to my application")
-}
+// func printWelcomeMessage(){
+// 	fmt.Println("Welcome to my application")
+// }
 
 //take input from user
-func getUserName() string { 
-	var userName string
-	fmt.Println("Enter your name: ")
-	fmt.Scanln(&userName)
-	return userName
-}
+// func getUserName() string {
+// 	var userName string
+// 	fmt.Println("Enter your name: ")
+// 	fmt.Scanln(&userName)
+// 	return userName
+// }
 
-func getTwoNumbers() (int, int) {
-	var numOne int
-	var numTwo int
-	fmt.Println("Enter number one: ")
-	fmt.Scanln(&numOne)
-	
-	fmt.Println("Enter number two: ")
-	fmt.Scanln(&numTwo)  //by &numTwo we are assigning numTwo to the variable
-	return numOne, numTwo
-}
+// func getTwoNumbers() (int, int) {
+// 	var numOne int
+// 	var numTwo int
+// 	fmt.Println("Enter number one: ")
+// 	fmt.Scanln(&numOne)
 
-func display(name string, sum int) {
-	fmt.Println("Hi",name,"!","Sum of two input numbers is: ", sum)
-}
+// 	fmt.Println("Enter number two: ")
+// 	fmt.Scanln(&numTwo)  //by &numTwo we are assigning numTwo to the variable
+// 	return numOne, numTwo
+// }
 
-func printGoodbyeMessage() {
-	fmt.Println("Thank you for using my application")
-    fmt.Println("Good bye")
-}
+// func display(name string, sum int) {
+// 	fmt.Println("Hi",name,"!","Sum of two input numbers is: ", sum)
+// }
+
+// func printGoodbyeMessage() {
+// 	fmt.Println("Thank you for using my application")
+//     fmt.Println("Good bye")
+// }
 
 //recursion
-func testcount(x int) int {
-	if x == 11 {
-	  return 0
-	}
-	fmt.Println(x)
-	return testcount(x + 1)
-  }
+// func testcount(x int) int {
+// 	if x == 11 {
+// 	  return 0
+// 	}
+// 	fmt.Println(x)
+// 	return testcount(x + 1)
+//   }
 
-func main(){
-// SOLID >>> S = Single Responsibility Principle >>> will apply to main function
-printWelcomeMessage()
-name := getUserName()
-n1,n2 := getTwoNumbers()
-sum := add(n1, n2)
-display(name, sum)
-printGoodbyeMessage()
+// func main(){
+// // SOLID >>> S = Single Responsibility Principle >>> will apply to main function
+// printWelcomeMessage()
+// name := getUserName()
+// n1,n2 := getTwoNumbers()
+// sum := add(n1, n2)
+// display(name, sum)
+// printGoodbyeMessage()
 
-testcount(5)
+// testcount(5)
+
+// }
+
+//class - 14 >>> scope
+
+var a = 20
+var b = 30
+
+func add(x int , y int){
+	z := x + y
+	fmt.Println(z)
+}
+func main() {
+	p := 30
+	q := 40
+	add(p, q)
+
+	add(a, b)
+
+	add(a, p)
+
+	// add(b, z)
 
 }
 
