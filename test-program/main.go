5ea9852bbebe37545e0bf1609f60ddc7afce5724
rashedevs <rasheduap2015@gmail.com>
2025-02-15 03:17:18 +0600

    practice hof and optimization

--- test-program/main.go
+++ test-program/main.go
@@ -1,10 +1,29 @@
-package main
+// package main
 
-import "fmt"
-  
-func main() {
-	fmt.Println("hello world", name)
-}
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("hello world", name)
+// }
 
 // to access and test the package level scope, wee need to run all the related files together.
 // like this: go run main.go add.go
+
+// practice test
+package main
+
+import "fmt"
+
+
+func EvenOrOdd(x int){
+    if x % 2 == 0{
+        fmt.Println("EVEN")
+    }else{
+        fmt.Println("ODD")
+    }
+
+}
+
+func main(){
+    EvenOrOdd(66)
+}
