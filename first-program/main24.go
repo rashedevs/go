6ceadac37b50a69c94552fcc5841431138f7fc33
rashedevs <rasheduap2015@gmail.com>
2025-02-22 03:53:57 +0600

    deep dive into run and build processes

--- first-program/main24.go
+++ first-program/main24.go
@@ -4,12 +4,14 @@ package main
 
 // Memory (RAM) Partioning:
 
-// 1. Code segment: The code segment is used to store the program's instructions.
+// 1. Code segment: The code segment is used to store the program's instructions and const variables.
+//    The code segment is read-only. Local function expressions are stored in the code segment.
 
-// 2. Data segment: The data segment is used to store the values of variables.
+// 2. Data segment: The data segment is used to store the values of re-assignable variables.
+//    These are allocated to ram's data segment at run/execution time.
 
 // 3. Stack: Place where the function call is stored. The stack is used to store
-//     the values of variables that are created at compile time.
+//    the values of variables that are created at compile time.
 
 //---- Stack Frame: A stack frame is a block of memory that is used to store
 //---- the values of variables that are created at runtime. Stack frames are dynamic in size.
