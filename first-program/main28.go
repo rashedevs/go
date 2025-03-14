ea1502be4dc052e15a9caa793b7bfb9f1a0a80f8
rashedevs <rasheduap2015@gmail.com>
2025-03-14 16:07:20 +0600

    explore array

--- first-program/main28.go
+++ first-program/main28.go
@@ -2,11 +2,10 @@ package main
 
 import "fmt"
 
-
-type User struct{
-	Name string                 //member variable or property
-	Age int
-}
+// type User struct{
+// 	Name string                 //member variable or property
+// 	Age int
+// }
 
 // normal function to print user details
 
@@ -16,37 +15,41 @@ type User struct{
 // }
 
 // receiver function is a function that is associated with a type
-func (usr User) printDetails(otherParam string){
-	fmt.Println(otherParam)
-	fmt.Println("Name:", usr.Name)
-	fmt.Println("Age:", usr.Age)
-}
+// func (usr User) printDetails(otherParam string){
+// 	fmt.Println(otherParam)
+// 	fmt.Println("Name:", usr.Name)
+// 	fmt.Println("Age:", usr.Age)
+// }
 
-func (usr User) call(a int){
-	fmt.Println("Amar jonmo", a)
-	fmt.Println("Name:", usr.Name)
-	fmt.Println("Age:", usr.Age)
-}
+// func (usr User) call(a int){
+// 	fmt.Println("Amar jonmo", a)
+// 	fmt.Println("Name:", usr.Name)
+// 	fmt.Println("Age:", usr.Age)
+// }
 
-func main() {
+// func main() {
 
-var user1 User
-user1 = User{
-	Name: "rashed",      
-	Age: 31,
-}
+// var user1 User
+// user1 = User{
+// 	Name: "rashed",
+// 	Age: 31,
+// }
 
-// printUserDetails(user1)
-user1.printDetails("Hello, I am a parameter for receiver function for user1 instance")
+// // printUserDetails(user1)
+// user1.printDetails("Hello, I am a parameter for receiver function for user1 instance")
 
-user2 := User{                                     
-	Name: "uzzaman",    
-	Age: 32,
-}
+// user2 := User{
+// 	Name: "uzzaman",
+// 	Age: 32,
+// }
 
-// printUserDetails(user2)
-user2.printDetails("Hello, I am a parameter for receiver function for user2 instance")
+// // printUserDetails(user2)
+// user2.printDetails("Hello, I am a parameter for receiver function for user2 instance")
 
-user2.call(1994)
+// user2.call(1994)
 
+// }
+
+func init(){
+	fmt.Println("This is init function")
 }
---
+++ first-program/main29.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var arr2 = [3]string{"I","love","you."}
+
+
+func main() {	
+
+	// var arr [2]int
+    // arr[1]=6
+	// arr[0]=3
+
+	//short array declare
+	arr := [2]int {3,6}
+	fmt.Println(arr)
+
+	fmt.Println(arr2[1])
+}
