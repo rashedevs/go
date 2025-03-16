5811a1f3d89404a233325c873281416fa23ee4c0
rashedevs <rasheduap2015@gmail.com>
2025-03-16 16:04:54 +0600

    array defination in all ways

--- first-program/main29.go
+++ first-program/main29.go
@@ -7,13 +7,15 @@ var arr2 = [3]string{"I","love","you."}
 
 func main() {	
 
-	// var arr [2]int
-    // arr[1]=6
-	// arr[0]=3
+	var arr1 [2]int
+	arr1[0]=3
+    arr1[1]=6
+	
 
 	//short array declare
-	arr := [2]int {3,6}
+	arr := [2]int{3,6}
 	fmt.Println(arr)
 
 	fmt.Println(arr2[1])
+	fmt.Println(arr2)
 }
