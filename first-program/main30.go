cffc851a3f84732572fc2d54ecdb769115accead
rashedevs <rasheduap2015@gmail.com>
2025-03-23 15:22:26 +0600

    address and value alltogether

--- first-program/main30.go
+++ first-program/main30.go
@@ -5,5 +5,10 @@ import "fmt"
 func main(){
 	// pointer or address of memory (ram)
 	x := 28
-	fmt.Println(x)
+    
+	// p holds the address of x
+	p := &x //ampersand x = address of x || 0xc000010170 (hex) ==> 211106249404416
+	fmt.Println("p holds the address of x variable =", p)
+	fmt.Println("value (of x) at the p address =", *p)   // * => value at address
+	fmt.Println("in short",*&x)
 }
