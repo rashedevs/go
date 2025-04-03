b8ab6c864d192bedc33d90027dab26c0f0f6a744
rashedevs <rasheduap2015@gmail.com>
2025-04-03 22:44:00 +0600

    exploring slice define stratagies

---
+++ first-program/main31-first.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+// func main(){
+// 	arr := [6]string{"This","is","a","go","interview","question"}
+//     fmt.Println(arr)
+
+// 	//# Ways of SLICE declaration ..............
+
+// 	// 1. slice from an array
+// 	s := arr[1:4]              // ["is","a","go"]
+// 	fmt.Println(s)
+
+// 	// 2. slice from a slice
+// 	s1 := s[1:2]              // ["a"]
+// 	fmt.Println(s1)
+// 	fmt.Println(len(s1))
+// 	fmt.Println(cap(s1))
+
+// 	// 3. Defining a new slice with slice literal
+// 	slice := []int{1,2,5}     // slice literal - (an array without size)
+// 	fmt.Println("slice",slice,"len",len(slice),"cap",cap(slice))
+
+// 	// 4. Using make function with length - (default capacity will be same as length)
+// 	// slice_make := make([]int,3)
+
+// 	// 5. Using make function with length & capacity
+// 	slice_make := make([]int,3, 5)
+// 	slice_make[0]=1
+// 	slice_make[2]=3
+// 	fmt.Println("slice with make function", slice_make, "len",len(slice_make),"cap", cap(slice_make))
+
+// 	// 6. Empty slice or nil slice
+// 	var empty_slice []int                      // [], len=0, cap=0
+// 	empty_slice = append(empty_slice, 1,2,3)   // [1,2,3], len=3, cap=3
+
+// 	fmt.Println(empty_slice)
+// }
+
+func init(){
+	fmt.Println("slice or 31st class in two file. here is from start to 1 hour 13 min")
+}
---
+++ first-program/main31-last.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+	// Empty slice or nil slice
+	var x []int                      // [], len=0, cap=0
+	x = append(x, 1)                 // [1], len=1, cap=1
+	x = append(x, 2)                 // [1,2], len=2, cap=2
+	x = append(x, 3)                 // [1,2,3], len=3, cap=3
+
+	fmt.Println(x)
+
+	y := x
+
+	x = append(x, 4)
+	y = append(y, 5)
+
+	x[0]=10
+
+	fmt.Println(x)
+	fmt.Println(y)
+}
