ca27550b7e93ea330c531467ee40bb4f3948e9ea
rashedevs <rasheduap2015@gmail.com>
2025-04-28 05:06:21 +0600

    explore variable shadowing

--- first-program/slice-practice.go
+++ first-program/slice-practice.go
@@ -8,25 +8,18 @@ import "fmt"
 // 	fmt.Printf("[addElement] after append addr of s[0]: %p\n", &s[0])
 // 	return s
 // }
-
+var y = 665  
 func main() {
-	// s := []int{1, 2, 3}
-	// fmt.Printf("[main---] before append addr of s[0]: %p\n", &s[0])
-	// s = addElement(s, 4)
-	// fmt.Printf("[main---] after append addr of s[0]: %p\n", &s[0])
+var x = 5  
+fmt.Println(x)
 
-	s := make([]int, 3, 5)
+{
+	x := 10
+fmt.Println(x)
+fmt.Println(y)
 
-	fmt.Println("s after initialization", &s[0])
-
-	s[0]=5
-	s[1]=15
-	s[2]=25
-	fmt.Println("s after index based insertion", &s[0])
-	// s[3]=30
-
-	s = append(s, 30)
-	fmt.Println("s after append", &s[0])
+}
+fmt.Println(x)
 }
 
 //1024 er pore 25% - 50% capacity increase hoite pare.
