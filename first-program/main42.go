de4c0eddb2c679a314583a82dfafccead2e76d4e
rashedevs <rasheduap2015@gmail.com>
2025-07-12 20:49:17 +0600

    slice on a glance

--- first-program/main42.go
+++ first-program/main42.go
@@ -21,23 +21,40 @@ func printHello(num int){
 }
 
 func main(){
-    var x int = 10
-	fmt.Println("hello", x)
+    // var x int = 10
+	// fmt.Println("hello", x)
 
-	go printHello(1)
+	// go printHello(1)
 
-	go printHello(2)
+	// go printHello(2)p[10]
 
-	go printHello(3)
+	// go printHello(3)
 
-	go printHello(4)
+	// go printHello(4)
 
-	go printHello(5)
+	// go printHello(5)
 
-	fmt.Println(a," ",pi)
+	// fmt.Println(a," ",pi)
 
-	time.Sleep(5*time.Second)
+	// time.Sleep(5*time.Second)
 
-    fmt.Println("haha")
+    // fmt.Println("haha")
 
+	p := []int{1,2,3,4,5}
+	p = append(p, 6)
+	p = append(p, 7)
+	q := p[4:]
+	q = append(q, 11)
+	q = append(q, 12)
+	q = append(q, 13)
+	q = append(q, 14)
+
+fmt.Printf("ok %p",&p)
+fmt.Println(len(p))
+fmt.Println(q)
+fmt.Println(cap(q))
+fmt.Println(p)
+
+fmt.Printf("okp %p\n",&p)
+fmt.Printf("okq %p",&q)
 }
