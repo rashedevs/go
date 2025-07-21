fa5b19032265efe2441714a86f35c9f537aea56e
rashedevs <rasheduap2015@gmail.com>
2025-07-22 01:40:39 +0600

    explored roundup size for byte class

--- first-program/main42.go
+++ first-program/main42.go
@@ -1,38 +1,82 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+
+// "time"
 
 //goroutine in complex way
 
-var a = 10
-const pi = 3.1416
+// var a = 10
+// const pi = 3.1416
 
 // func add(a,b int){
 // 	fmt.Println(a+b)
 // }
 
-func printHello(num int){
-	time.Sleep(5*time.Second)
-	fmt.Println("Hello rashed",num)
-	// add(4,5)
-}
+// func printHello(num int){
+// 	time.Sleep(5*time.Second)
+// 	fmt.Println("Hello rashed",num)
+// 	// add(4,5)
+// }
 
 func main() {
+	// var x []int          // [], ptr=nil, len=0, cap=0
 
-slc := []int{}
+	// x = append(x, 1)  
 
-slc = append(slc, 10)
+	// x = append(x, 2)
+	// fmt.Println(len(x))
+	// fmt.Println(cap(x))
 
-fmt.Print(cap(slc))
+	// x = append(x, 2, 3, 4, 5, 6, 7)
 
-slc = append(slc, 13, 14)
+	// x = append(x, 2, 3, 4, 5, 6, 7)	
+	// fmt.Println(len(x))
+	// fmt.Println(cap(x))
 
-fmt.Print(cap(slc))
 
-slc = append(slc, 16)
 
-fmt.Print(cap(slc))
+
+
+	// for i := 1; i <= 1025; i++{
+	// 	x = append(x, i)
+	// 	fmt.Printf("After %d: len=%d cap=%d\n", i, len(x), cap(x))
+	// }
+
+
+	// x = append(x, 3)	
+	// fmt.Println(len(x))
+	// fmt.Println(cap(x))
+
+	// y := x
+	// x = append(x, 4)	
+	// y = append(x, 5)	
+
+	// x[0] = 10	
+
+	// fmt.Println(x)  
+	// fmt.Println(y)   
+	
+	
+	var slice []int
+    prevCap := cap(slice)
+
+    fmt.Printf("%-10s | %-10s | %-6s | %-8s | %-10s\n", "Length", "Capacity", "Δ", "Growth", "× Increase")
+
+    for i := 0; i < 3500; i++ {
+        slice = append(slice, i)
+        newCap := cap(slice)
+        if newCap != prevCap {
+            delta := newCap - prevCap
+            growthPercent := 0.0
+            multiplier := "-"
+            if prevCap != 0 {
+                growthPercent = float64(delta) / float64(prevCap) * 100
+                multiplier = fmt.Sprintf("%.2fx", float64(newCap)/float64(prevCap))
+            }
+            fmt.Printf("Length: %-4d  Capacity: %-4d     |   Δ: %-4d   |   Growth: %5.1f%%    |  %-10s\n",
+                len(slice), newCap, delta, growthPercent, multiplier)
+            prevCap = newCap
+        }
+    }
 }
--- first-program/slice-practice.go
+++ first-program/slice-practice.go
@@ -7,19 +7,6 @@ package main
 // 	return s
 // }
 
-// func main() {
-// // variable shadowing
-// var x = 5
-// fmt.Println(x)
-
-// {
-// 	x := 10
-// fmt.Println(x)
-
-// }
-// fmt.Println(x)
-// }
-
 // 1024 er pore 25% - 50% capacity increase hoite pare.
 // func main() {
 //     s := make([]int, 1024)
@@ -32,12 +19,46 @@ package main
 // len: 1024, cap: 1024
 // len: 1025, cap: 1536
 
-// func main() {
-// var i uint8 = 255
-// i += 10
-// fmt.Println(i)
+// slice class practice here below,
 
-// var i int8 = 120
-// i += 16
-// fmt.Println(i)
+// func main(){
+// arr := [6]string{"this","is","a","go","interview","question"}
+
+// // 1. slice from an array
+// slice1 := arr[1:4]
+// fmt.Println("slice1", slice1)  //[is a go]
+// fmt.Println(len(slice1))       // 3
+// fmt.Println(cap(slice1))       // 5
+
+// //2. slice from a slice
+// slice2 := slice1[1:2]
+// fmt.Println("slice2", slice2)  // [a]
+// fmt.Println(len(slice2))       // 1
+// fmt.Println(cap(slice2))       // 4
+
+// //3. slice literals (initially len and cap same)
+// slc := []int{1,2,3}
+// fmt.Println("slc", slc)  // [1 2 3]
+// fmt.Println(len(slc))       // 3
+// fmt.Println(cap(slc))       // 3
+
+// //4. slice using make function with length
+// mslc := make([]int, 3)
+// fmt.Println("mslc", mslc)  // [0 0 0]
+// fmt.Println(len(mslc))       // 3
+// fmt.Println(cap(mslc))       // 3
+
+// //5. slice using make function with length & cap
+// mslc2 := make([]int, 3, 5)
+// fmt.Println("mslc2", mslc2)  // [0 0 0]
+// fmt.Println(len(mslc2))       // 3
+// fmt.Println(maincap(mslc2))       // 3
+
+// //6. empty / nil slice (initially, ptr = nil, length = 0 & cap = 0)
+// nilslc := []int{}
+// fmt.Println("nilslc", nilslc)  // []
+// fmt.Println(len(nilslc))       // 0
+// fmt.Println(cap(nilslc))       // 0
+
 // }
+
