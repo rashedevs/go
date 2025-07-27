bceb345a6963fb29c9e0d295f02b147e40386907
rashedevs <rasheduap2015@gmail.com>
2025-07-27 16:22:40 +0600

    growslice with roundupsize

--- first-program/main42.go
+++ first-program/main42.go
@@ -19,20 +19,27 @@ import "fmt"
 // 	// add(4,5)
 // }
 
+
 func main() {
-	// var x []int          // [], ptr=nil, len=0, cap=0
+	// var x []int          // [], ptr = nil, len=0, cap=0
 
-	// x = append(x, 1)  
+	x := make([]int, 512)
 
+	// x = append(x, 1)   
 	// x = append(x, 2)
-	// fmt.Println(len(x))
-	// fmt.Println(cap(x))
+	// x = append(x, 3)
+	// x = append(x, 4)
+	// x = append(x, 5)
+	// x = append(x, 6)
+	// x = append(x, 7)
+	// x = append(x, 8)
+	// x = append(x, 9)
 
-	// x = append(x, 2, 3, 4, 5, 6, 7)
+	
 
-	// x = append(x, 2, 3, 4, 5, 6, 7)	
-	// fmt.Println(len(x))
-	// fmt.Println(cap(x))
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
 
 
 
@@ -58,25 +65,23 @@ func main() {
 	// fmt.Println(y)   
 	
 	
-	var slice []int
-    prevCap := cap(slice)
-
-    fmt.Printf("%-10s | %-10s | %-6s | %-8s | %-10s\n", "Length", "Capacity", "Δ", "Growth", "× Increase")
-
-    for i := 0; i < 3500; i++ {
-        slice = append(slice, i)
-        newCap := cap(slice)
-        if newCap != prevCap {
-            delta := newCap - prevCap
-            growthPercent := 0.0
-            multiplier := "-"
-            if prevCap != 0 {
-                growthPercent = float64(delta) / float64(prevCap) * 100
-                multiplier = fmt.Sprintf("%.2fx", float64(newCap)/float64(prevCap))
-            }
-            fmt.Printf("Length: %-4d  Capacity: %-4d     |   Δ: %-4d   |   Growth: %5.1f%%    |  %-10s\n",
-                len(slice), newCap, delta, growthPercent, multiplier)
-            prevCap = newCap
-        }
-    }
+	// var slice []int
+    // prevCap := cap(slice)
+    // fmt.Printf("%-10s | %-10s | %-6s | %-8s | %-10s\n", "Length", "Capacity", "Δ", "Growth", "× Increase")
+    // for i := 0; i < 25000; i++ {
+    //     slice = append(slice, i)
+    //     newCap := cap(slice)
+    //     if newCap != prevCap {
+    //         delta := newCap - prevCap
+    //         growthPercent := 0.0
+    //         multiplier := "-"
+    //         if prevCap != 0 {
+    //             growthPercent = float64(delta) / float64(prevCap) * 100
+    //             multiplier = fmt.Sprintf("%.2fx", float64(newCap)/float64(prevCap))
+    //         }
+    //         fmt.Printf("Length: %-4d  Capacity: %-4d     |   Δ: %-4d   |   Growth: %5.1f%%    |  %-10s\n",
+    //             len(slice), newCap, delta, growthPercent, multiplier)
+    //         prevCap = newCap
+    //     }
+    // }
 }
